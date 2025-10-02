e34f2d4a8365146ed9040832f6ee03c1bc4e74db
DaDodoDev <williamharenkat@gmail.com>
2025-10-02 12:19:50 +0200

    drownedAnim

--- Assets/_Scripts/drownedScript.cs
+++ Assets/_Scripts/drownedScript.cs
@@ -14,6 +14,15 @@ public class drownedScript : MonoBehaviour
     
     public float distanceToPlayer;
     public float runAwayDistance;
+    
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] upSprites;
+    public Sprite[] downSprites;
+    public Sprite[] leftSprites;
+    public Sprite[] rightSprites;
+
+    public float frame;
+    public float framesPerSecond;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,9 +32,24 @@ public class drownedScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        frame += Time.deltaTime * framesPerSecond;
+        if(frame >= downSprites.Length){frame = 0;}
+        Vector2 direction = (player.position - transform.position).normalized;
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if(direction.x > 0){spriteRenderer.sprite = rightSprites[(int)frame];}
+            else{spriteRenderer.sprite = leftSprites[(int)frame];}
+        }
+        else
+        {
+            if(direction.y > 0){spriteRenderer.sprite = upSprites[(int)frame];}
+            else{spriteRenderer.sprite = downSprites[(int)frame];}
+        }
         if (Vector2.Distance(transform.position, player.position) > distanceToPlayer)
         {
+            
             rb.linearVelocity = (player.position-transform.position).normalized * speed ;
+            
         }
         else if (Vector2.Distance(transform.position, player.position) < runAwayDistance)
         {
